4d394b97ebc05a94bc826d4be3c46bcd38c4a64b
satvikpathak007 <satvikpathak007@gmail.com>
2024-11-10 02:38:27 +0530

    Initial commit: Added base structure and files for portfolio website, including HTML, CSS, and setup configurations.

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='overflow-x-hidden text-neutral-300 antialised slec selection:bg-cyan-300 selection:text-cyan-900'>
+      <div className='flixed top-0 -z-10 h-full w-full'>
+        <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+        </div>
+      </div>
+      
+      <div className='container mx-auto px-8 '>
+        <Navbar />
+        <Hero />
+      </div>
+    </div>
+  )
+}
+
+export default App
